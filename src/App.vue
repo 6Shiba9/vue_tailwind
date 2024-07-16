636737245a5b00feb07765c5340cd8c277bf70f1
Thanachai Yatar <cuter_1911_@hotmail.com>
2024-07-17 03:16:29 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import alert_bar from './components/alert_bar.vue';
+import navbar from './components/navbar.vue';
+</script>
+<template lang="">
+  <div>
+  <alert_bar/>
+  </div>
+  <div>
+<navbar/>
+  </div>
+</template>
+
+<style lang="">
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex w-[1200px] h-[42px] px-5 p-10 items-center justify-center mx-auto">
+<div class="flex gap-2 flex-1">
+    <img src="../assets/img/logo.png"class="h-5 ">LearningHub
+</div>
+<div class="flex gap-5 flex-1">
+        <div herf="#">
+            Code
+        </div>
+        <div herf="#">
+            Design
+        </div>
+        <div herf="#">
+            Prototype
+        </div>
+</div>
+<div class="flex">
+<img src="../assets/img/sc.png" class="h-4">
+</div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
